accc2a7d9aaffc8e01da259c0ed5273f93b154be
Anastasiia Vudvud <anastasiiavudvud@gmail.com>
2023-11-12 16:55:52 +0100

    deleted unnecessary things

--- src/lab5/Maze.java
+++ src/lab5/Maze.java
@@ -1,23 +1,12 @@
 package lab5;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.awt.*;
-import javax.swing.*;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.ArrayList;
 
-/**
- * Creates a random maze, then solves it by finding a path from the
- * upper left corner to the lower right corner. (After doing
- * one maze, it waits a while then starts over by creating a
- * new random maze.)
- */
 public class Maze implements Runnable {
 
     // a main routine makes it possible to run this class as a program
@@ -26,45 +15,15 @@ public class Maze implements Runnable {
     }
 
 
-    int[][] maze; // Description of state of maze. The value of maze[i][j]
-    // is one of the constants wallCode, pathcode, emptyCode,
-    // or visitedCode. (Value can also be negative, temporarily,
-    // inside createMaze().)
-    // A maze is made up of walls and corridors. maze[i][j]
-    // is either part of a wall or part of a corridor. A cell
-    // cell that is part of a corridor is represented by pathCode
-    // if it is part of the current path through the maze, by
-    // visitedCode if it has already been explored without finding
-    // a solution, and by emptyCode if it has not yet been explored.
+    int[][] maze;
 
     final static int backgroundCode = 0;
     final static int wallCode = 100;
-    final static int pathCode = 2;
-    final static int emptyCode = 3;
-    final static int visitedCode = 4;
 
     private static final int WALL = 100;
     private static final int CORRIDOR = 0;
     private static final int PATH = 200;
 
-    int rows = 41; // number of rows of cells in maze, including a wall around edges
-    int columns = 41; // number of columns of cells in maze, including a wall around edges
-    int border = 0; // minimum number of pixels between maze and edge of panel
-    int sleepTime = 5000; // wait time after solving one maze before making another
-    int speedSleep = 30; // short delay between steps in making and solving maze
-    int blockSize = 12; // size of each cell
-
-    int width = -1; // width of panel, to be set by checkSize()
-    int height = -1; // height of panel, to be set by checkSize()
-
-    int totalWidth; // width of panel, minus border area (set in checkSize())
-    int totalHeight; // height of panel, minus border area (set in checkSize())
-    int left; // left edge of maze, allowing for border (set in checkSize())
-    int top; // top edge of maze, allowing for border (set in checkSize())
-
-    boolean mazeExists = false; // set to true when maze[][] is valid; used in
-    // redrawMaze(); set to true in createMaze(), and
-    // reset to false in run()
 
     public void createNewImage() {
         int width = maze.length;
@@ -210,14 +169,6 @@ public class Maze implements Runnable {
 
 
     public void run() {
-        // run method for thread repeatedly makes a maze and then solves it
-//        try {
-//            Thread.sleep(1000);
-//        } // wait a bit before starting
-//        catch (InterruptedException e) {
-//        }
-//        while (true) {
-//            makeMaze();
 
 
             loadMaze();
@@ -230,49 +181,5 @@ public class Maze implements Runnable {
         }
         createNewImage();
 
-
-//            System.out.println(solveMaze(1,0));
-//            System.out.println();
-//            System.out.println();
-//            System.out.println();
-//            printMaze();
-//            synchronized (this) {
-//                try {
-//                    wait(sleepTime);
-//                } catch (InterruptedException e) {
-//                }
-//            }
-//            mazeExists = false;
-//            repaint();
-//        }
     }
-
-//    boolean solveMaze(int row, int col) {
-//        // Try to solve the maze by continuing current path from position
-//        // (row,col). Return true if a solution is found. The maze is
-//        // considered to be solved if the path reaches the lower right cell.
-//        if (maze[row][col] == emptyCode) {
-//            maze[row][col] = pathCode; // add this cell to the path
-//            if (row == rows - 2 && col == columns - 1)
-//                return true; // path has reached goal
-//            try {
-//                Thread.sleep(speedSleep);
-//            } catch (InterruptedException e) {
-//            }
-//            if (solveMaze(row - 1, col) || // try to solve maze by extending path
-//                    solveMaze(row, col - 1) || // in each possible direction
-//                    solveMaze(row + 1, col) ||
-//                    solveMaze(row, col + 1))
-//                return true;
-//            // maze can't be solved from this cell, so backtrack out of the cell
-//            maze[row][col] = visitedCode; // mark cell as having been visited
-//            synchronized (this) {
-//                try {
-//                    wait(speedSleep);
-//                } catch (InterruptedException e) {
-//                }
-//            }
-//        }
-//        return false;
-//    }
 }
